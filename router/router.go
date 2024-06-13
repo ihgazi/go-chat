a78f40be40631d7d1182c645cdcc6f9692d6a373
DeathKrow <irfanhgazi@gmail.com>
2024-06-13 22:06:31 +0530

    FEAT: Added user handler, router - Implemented user handler to create user create endpoint - Creater router to configure endpoints - Added gin-gonic dependency

---
+++ router/router.go
@@ -0,0 +1,20 @@
+package router 
+
+import (
+    "github.com/ihgazi/go-chat/internal/user"
+    "github.com/gin-gonic/gin"
+)
+
+// GIN Router to create API endpoints
+
+var r *gin.Engine
+
+func InitRouter(userHandler *user.Handler) {
+    r = gin.Default()
+
+    r.POST("/signup", userHandler.CreateUser)
+}
+
+func Run(addr string) error {
+    return r.Run(addr)
+}
