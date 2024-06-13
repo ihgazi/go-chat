567e66f65b103252490d9d1353969986a13e9aa4
DeathKrow <irfanhgazi@gmail.com>
2024-06-13 22:01:09 +0530

    FEAT: Create repository and service dependency for user

---
+++ internal/user/user_service.go
@@ -0,0 +1,47 @@
+package user
+
+import (
+    "time"
+    "strconv"
+    "context"
+)
+
+// User service takes CreateUserReq object from API endpoint
+// Creates a User struct and passes it to repository
+
+type service struct {
+    Repository
+    timeout time.Duration
+}
+
+func NewService(repository Repository) Service {
+    return &service{
+        repository,
+        time.Duration(2) * time.Second,
+    }
+}
+
+func (s *service) CreateUser(c context.Context, req *CreateUserReq) (*CreateUserRes, error) {
+    ctx, cancel := context.WithTimeout(c, s.timeout)
+    defer cancel()
+
+    // TODO: Implement password hashing
+    u := &User{
+        Username: req.Username,
+        Email: req.Email,
+        Password: req.Password,
+    }
+
+    r, err := s.Repository.CreateUser(ctx, u)
+    if err != nil {
+        return nil, err
+    }
+
+    res := &CreateUserRes{
+        ID: strconv.Itoa(int(r.ID)),
+        Username: r.Username,
+        Email: r.Email,
+    }
+
+    return res, nil;
+}
